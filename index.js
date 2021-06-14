767b12b7c894a4a5aef66f1b9820b2a4c0f8729c
iasjem <jemimah.beryl.sai@gmail.com>
2021-06-14 21:56:57 +0800

    Modify value for option All

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const iframePage = document.getElementById("page");
 const BASE_URL = "https://arcane-mesa-16302.herokuapp.com/bar-chart";
 
 selectFilterOptions.addEventListener("change", function () {
-  let filterRegion = selectFilterOptions.value;
+  let filterRegion = selectFilterOptions.value !== undefined ? selectFilterOptions.value : "ALL";
 
-  iframePage.src = filterRegion !== "ALL" ? BASE_URL + "?region=" + filterRegion : iframePage.src = BASE_URL;
+  iframePage.src = filterRegion.toUpperCase() !== "ALL" ? BASE_URL + "?region=" + filterRegion : iframePage.src = BASE_URL;
 });
